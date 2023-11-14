293690f2e4eb659fd77f73c9f544ef01026f414e
carlosmsb <82188401+carlosmsb@users.noreply.github.com>
2023-11-14 09:33:44 -0800

    Add files via upload

---
+++ app.js
@@ -0,0 +1,19 @@
+//const message = require('./message')
+//console.log(message)
+//console.log(message.name)
+//console.log(message.role)
+
+//const Message = require('./message')
+//const message1 = new Message('Zak Ruvalcaba', 'Instructor')
+//message1.sayHello()
+
+//import Message from './message.js'
+//const message1 = new Message('Zak Ruvalcaba', 'Instructor')
+//message1.sayHello()
+
+// Module wrapper function
+(function (exports, require, module, __filename, __dirname) {
+
+})
+
+console.log(__dirname, __filename)
